27cb561bf5a27e06a786f2da37cc652352a7242c
Keith Lancaster <klancaster1957@acm.org>
2014-11-07 15:22:56 -0600

    basic tests in place

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,10 +2,24 @@ class Account < ActiveRecord::Base
 
   after_initialize :init_balance
 
+
+  def deposit_amount(amount)
+    self.balance += amount
+  end
+
+  def withdraw_amount(amount)
+    if amount < self.balance
+      self.balance -= amount
+    else
+      false
+    end
+  end
+
   private
 
   def init_balance
     self.balance ||= 0
   end
+
 
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -8,4 +8,21 @@ class AccountTest < ActiveSupport::TestCase
   test "An account with no deposits should return 0 for its amount" do
     assert_equal 0,@account.balance
   end
+
+  test "An account with one deposit should have a balance equal to that deposit" do
+    @account.deposit_amount(10)
+    assert_equal 10, @account.balance
+  end
+
+  test "Withdrawing from the account should alter the balance" do
+    @account.deposit_amount(100)
+    assert @account.withdraw_amount(10)
+    assert_equal 90, @account.balance
+  end
+
+  test "A user cannot withdraw more than what is in the account" do
+    @account.deposit_amount(10)
+    assert  !@account.withdraw_amount(100), "Withdrawal incorrectly allowed"
+    assert_equal 10, @account.balance
+  end
 end
